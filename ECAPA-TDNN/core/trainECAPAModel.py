c3ae2dbbb16cbcd281608320667183447196c2d4
datnguyen0328 <88551675+datnguyen0328@users.noreply.github.com>
2024-11-25 13:50:39 +0700

    change --train_path

--- ECAPA-TDNN/core/trainECAPAModel.py
+++ ECAPA-TDNN/core/trainECAPAModel.py
@@ -23,7 +23,7 @@ parser.add_argument("--lr_decay",   type=float, default=0.97,    help='Learning 
 
 ## Training and evaluation path/lists, save path
 parser.add_argument('--train_list', type=str,   default="/kaggle/working/vietnam-path-id.txt",     help='The path of the training list, https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/train_list.txt')
-parser.add_argument('--train_path', type=str,   default="/kaggle/input/vietceleb/vietnam-celeb-part/data",                    help='The path of the training data, eg:"/data08/VoxCeleb2/train/wav" in my case')
+parser.add_argument('--train_path', type=str,   default="/kaggle/input/vietnam-celeb-dataset/full-dataset/data",                    help='The path of the training data, eg:"/data08/VoxCeleb2/train/wav" in my case')
 parser.add_argument('--eval_list',  type=str,   default="/kaggle/working/vietnam-eval-path.txt",              help='The path of the evaluation list, veri_test2.txt comes from https://www.robots.ox.ac.uk/~vgg/data/voxceleb/meta/veri_test2.txt')
 parser.add_argument('--eval_path',  type=str,   default="/kaggle/input/vietnam-celeb-dataset/full-dataset/data",                    help='The path of the evaluation data, eg:"/data08/VoxCeleb1/test/wav" in my case')
 parser.add_argument('--musan_path', type=str,   default="",                    help='The path to the MUSAN set, eg:"/data08/Others/musan_split" in my case')
